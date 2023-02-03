f5b5fcf96213ff8452b2a0efc9550519d01b020a
Danny Salman <salman.danny03@hotmail.com>
2023-02-02 23:24:04 -0500

    named function

--- assets/js/ipldsch.js
+++ assets/js/ipldsch.js
@@ -1,60 +1,62 @@
-export const ipldsch = {
-  case_insensitive: true,
-  illegal: ':',
-  contains: [
-    {
-      className: 'keyword',
-      begin: /^[ \t]*(?:type|advanced)[ \t][A-Z](_?[A-Za-z0-9])*\b/m,
-      contains: [
-        {
-          className: 'keyword',
-          begin: /^[ \t]*(type|advanced)/m,
-        },
-        {
-          className: 'class-name',
-          begin: /[\w]+$/,
-        },
-      ],
-    },
-    {
-      className: 'keyword',
-      begin: /\b(bool|int|float|string|bytes|null|nullable|optional)\b/,
-    },
-    {
-      className: 'builtin',
-      begin: /\b(struct|union|enum)(?=[ \t]*\{)\b/,
-    },
-    {
-      begin: /^}[ \t]representation\b/m,
-      contains: [
-        {
-          className: 'builtin',
-          begin: /representation/,
-        },
-      ],
-    },
-    {
-      className: 'operator',
-      begin: /=/,
-    },
-    {
-      className: 'number',
-      begin: /\b-?\d+\.?\d*(?:e[+-]?\d+)?\b/i,
-    },
-    {
-      className: 'punctuation',
-      begin: /[(){}:[\]|&]/,
-    },
-    {
-      className: 'string',
-      begin: /(")(?:\\[\s\S]|(?!\1)[^\\])*\1/,
-      greedy: true,
-    },
-    {
-      className: 'comment',
-      begin: /(^|[^"])#.*/,
-      lookbehind: true,
-      greedy: true,
-    },
-  ],
+export const ipldsch = function() {
+  return {
+    case_insensitive: true,
+    illegal: ':',
+    contains: [
+      {
+        className: 'keyword',
+        begin: /^[ \t]*(?:type|advanced)[ \t][A-Z](_?[A-Za-z0-9])*\b/m,
+        contains: [
+          {
+            className: 'keyword',
+            begin: /^[ \t]*(type|advanced)/m,
+          },
+          {
+            className: 'class-name',
+            begin: /[\w]+$/,
+          },
+        ],
+      },
+      {
+        className: 'keyword',
+        begin: /\b(bool|int|float|string|bytes|null|nullable|optional)\b/,
+      },
+      {
+        className: 'builtin',
+        begin: /\b(struct|union|enum)(?=[ \t]*\{)\b/,
+      },
+      {
+        begin: /^}[ \t]representation\b/m,
+        contains: [
+          {
+            className: 'builtin',
+            begin: /representation/,
+          },
+        ],
+      },
+      {
+        className: 'operator',
+        begin: /=/,
+      },
+      {
+        className: 'number',
+        begin: /\b-?\d+\.?\d*(?:e[+-]?\d+)?\b/i,
+      },
+      {
+        className: 'punctuation',
+        begin: /[(){}:[\]|&]/,
+      },
+      {
+        className: 'string',
+        begin: /(")(?:\\[\s\S]|(?!\1)[^\\])*\1/,
+        greedy: true,
+      },
+      {
+        className: 'comment',
+        begin: /(^|[^"])#.*/,
+        lookbehind: true,
+        greedy: true,
+      },
+    ],
+  };
 };

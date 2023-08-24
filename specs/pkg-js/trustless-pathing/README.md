c6a74fbece12886e32ff906c6ef2d97aa568b782
Rod Vagg <rod@vagg.org>
2023-08-24 21:08:59 +1000

    chore: publish as @ipld/specs

---
+++ specs/pkg-js/trustless-pathing/README.md
@@ -0,0 +1,35 @@
+# Trustless Pathing Fixtures for JS
+
+```js
+import {
+  unixfs20mVarietyCases,
+  unixfs20mVarietyCar
+} from '@ipld/specs/trustless-pathing/unixfs_20m_variety.js'
+
+const testCases = await unixfs20mVarietyCases()
+const carUrl = unixfs20mVarietyCar()
+
+// load the CAR using @ipld/car streamed from `carUrl` as a file
+// use `testCases` to run the tests
+```
+
+`unixfs20mVarietyCases()` returns an array of test cases that look like this:
+
+```ts
+class TestCase {
+    name: string
+    root: CID
+    path: string
+    scope: string
+    duplicates: boolean
+    byteRange: string
+    expectedCids: CID[]
+}
+```
+
+`TestCase#asQuery()` will print a Trustless Gateway style URL string representing the case, e.g.:
+
+```js
+console.log(tc.asQuery())
+// -> /ipfs/bafybeifrrglx2issn2had5rtstn3xltla6vxmpjfwfz7o3hapvkynh4zoq/clippet.txt?dag-scope=entity&entity-bytes=0:256144
+```
---
+++ specs/pkg-js/trustless-pathing/unixfs_20m_variety.js
@@ -0,0 +1,30 @@
+import { readFile } from 'node:fs/promises'
+import { parseCase } from './testcase.js'
+import { parse, index, patch, toString } from 'testmark.js'
+
+const file = '../../transport/trustless-pathing/fixtures/unixfs_20m_variety.'
+
+function pathToFixture(typ) {
+  return new URL(file + typ, import.meta.url)
+}
+
+export function unixfs20mVarietyCar() {
+  return pathToFixture('car')
+}
+
+export async function unixfs20mVarietyCases() {
+  const file = pathToFixture('md')
+  const contents = await readFile(file, 'utf-8')
+  const dirEnt = index(parse(contents))
+  const testCases = []
+  for (const test of dirEnt.children.get('test').childrenList) {
+    for (const scope of test.childrenList) {
+      testCases.push(parseCase(`${test.name}/${scope.name}`, dstr(scope, 'query'), dstr(scope, 'execution')))
+    }
+  }
+  return testCases
+}
+
+function dstr(dir, ch) {
+  return dir.children.get(ch).hunk.body
+}
